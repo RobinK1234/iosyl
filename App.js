278f9295e6ca35adef2d83be3dac7c1f35731788
Robin Kriisa <robinkriisa@Robins-MacBook-Air.local>
2023-10-04 13:50:17 +0300

    mhm

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { SafeAreaView } from "react-native";
 import Splash from "./src/screens/auth/Splash";
 import Signup from "./src/screens/auth/Signup";
 
+
 const App = () => {
   return (
     <SafeAreaView>
